e6ceaba0afeef304745a92c7c3d522695c613bec
Patrick Sandquist <60276286+metamoes@users.noreply.github.com>
2024-02-05 09:25:39 -0600

    Check for administrator privilege
    Curl requires admin privilege, added a more descriptive error message for when not admin

--- Install.bat
+++ Install.bat
@@ -3,6 +3,16 @@
 set "download_link=https://mirrors.gigenet.com/mariadb//mariadb-11.4.0/winx64-packages/mariadb-11.4.0-winx64.msi"
 set "filename=mariadb-11.4.0-winx64.msi"
 
+:: Determine if run as administrator
+if defined SessionName (
+  echo You are not running as administrator.
+  msg * "You are not running as administrator. Please check installation settings"
+  goto :error
+) else (
+  echo You are running as administrator.
+)
+
+
 :: Check for MariaDB installation
 reg query "HKLM\SOFTWARE\MariaDB" /v InstalledVersion >nul 2>&1
 if %errorlevel% equ 0 goto check_csv_files

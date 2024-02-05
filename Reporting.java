e2fe98241545395d420db616395fd60aeb4d4be8
stubbygermy <157933297+stubbygermy@users.noreply.github.com>
2024-02-04 22:58:42 -0600

    Added code for Generating Reports

--- Reporting.java
+++ Reporting.java
@@ -1,11 +1,36 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class Reporting extends JPanel {
-
-    public Reporting() {
+    private ArrayList<String> reports;
+    public JButton printReportButton;
+    
+    public Reporting(ArrayList<String> reports) {
+        this.reports = reports;
+        
         JPanel mainPanel = new JPanel();
         add(mainPanel);
         JLabel label = new JLabel("Reporting");
         mainPanel.add(label);
+        
+        printReportButton = new JButton("Print Report");
+        printReportButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printReports();
+            }
+        });
+    
+        mainPanel.add(printReportButton);
+    }
+    
+    public void printReports() {
+        System.out.println("Scanned IP Addresses: ");
+        for(String ipAddress : reports) {
+            System.out.println(ipAddress);
+        }
     }
 }

4807f69c9afc7511c183e0091399a29857315fec
Patrick Sandquist <60276286+metamoes@users.noreply.github.com>
2024-02-07 13:34:30 -0600

    added dependency
    added java as dependency and states confirmation of all installations

--- Install.bat
+++ Install.bat
@@ -1,8 +1,8 @@
 @echo off
 
 cd /d "%~dp0"
-set "download_link=https://mirrors.gigenet.com/mariadb//mariadb-11.4.0/winx64-packages/mariadb-11.4.0-winx64.msi"
-set "filename=mariadb-11.4.0-winx64.msi"
+set "Java_download_link=https://download.oracle.com/java/21/latest/jdk-21_windows-x64_bin.msi"
+set "Java_filename=jdk-21_windows-x64_bin.msi"
 
 :: Determine if run as administrator
 if defined SessionName (
@@ -14,21 +14,52 @@ if defined SessionName (
 )
 pause
 
+
+:Check if Java is installed:
+java -version 2>NUL
+
+:If Java is not installed, print a message and exit the script
+if ERRORLEVEL 1 (
+
+
+:Download Java installer: 
+cd installers
+curl -L "%Java_download_link%" -o "./%Java_filename%"
+
+if exist "/%Java_filename%" (
+  echo Download complete! File saved to "\%Java_filename%".
+) else (
+  echo Error: Download failed! Please check the download link and try again.
+)
+
+:Install Java: 
+start /wait msiexec.exe /i %Java_filename%
+cd ..
+)
+
+echo Java is installed.
+
+
+set "Maria_download_link=https://mirrors.gigenet.com/mariadb//mariadb-11.4.0/winx64-packages/mariadb-11.4.0-winx64.msi"
+set "Maria_filename=mariadb-11.4.0-winx64.msi"
+
+
+
 :Check for MariaDB installation: 
-IF EXIST "C:\Program Files\MariaDB 11.4" goto Launch_Howitzer
+IF EXIST "C:\Program Files\MariaDB 11.4" echo Maria is installed && goto Launch_Howitzer
 
 :Download MariaDB installer: 
 cd installers
-curl -L "%download_link%" -o "./%filename%"
+curl -L "%Maria_download_link%" -o "./%Maria_filename%"
 
-if exist "/%filename%" (
-  echo Download complete! File saved to "\%filename%".
+if exist "/%Maria_filename%" (
+  echo Download complete! File saved to "\%Maria_filename%".
 ) else (
   echo Error: Download failed! Please check the download link and try again.
 )
 
 :Install MariaDB: 
-start /wait msiexec.exe /i mariadb-11.4.0-winx64.msi
+start /wait msiexec.exe /i &Maria_filename&
 cd ..
 
 :Launch_Howitzer:

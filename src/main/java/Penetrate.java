20b52bd7306baae69da1d82f19d4fc9f626bc8ba
stubbygermy <157933297+stubbygermy@users.noreply.github.com>
2024-03-27 13:56:38 -0500

    Adding a couple of changes to get a bit of a start for the Pen Tab

--- src/main/java/Penetrate.java
+++ src/main/java/Penetrate.java
@@ -1,11 +1,30 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class Penetrate extends JPanel {
+    private ArrayList<String> detectedVulns;
+    private ArrayList<String> currentIPs;
 
-    public Penetrate() {
+    public Penetrate(ArrayList<String> vulnerabilities, ArrayList<String> currentIPs) {
+        this.detectedVulns = vulnerabilities;
+        this.currentIPs = currentIPs;
         JPanel mainPanel = new JPanel();
         add(mainPanel);
         JLabel label = new JLabel("Penetrate");
         mainPanel.add(label);
+
+        for (String vulnerability : detectedVulns) {
+            JButton vulnButton = new JButton(vulnerability);
+            mainPanel.add(vulnButton);
+            vulnButton.addActionListener(e -> {
+                JOptionPane.showMessageDialog(null, "Exploiting " + vulnerability);
+            });
+        }
+
+        JButton penButton = new JButton("Penetrate");
+        mainPanel.add(penButton);
+        penButton.addActionListener(e -> {
+            JOptionPane.showMessageDialog(null, "Penetrating with exploits");
+        });
     }
 }
